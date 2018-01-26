6b56394f01fe76b7f1175056d6ac7993f01273fb
alexjgutzwiller <alexjgutzwiller@gmail.com>
2018-01-25 23:07:49 -0500

    updated

--- config/routes.rb
+++ config/routes.rb
@@ -7,7 +7,7 @@ Rails.application.routes.draw do
 
   resources :quotes
   get 'static/pages'
+  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   
   root 'quotes#index'
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
